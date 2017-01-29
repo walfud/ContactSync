23d14565814caa077d5781c0ac6d0316cf22c136
walfud <walfud@walfuds-MacBook-Air.local>
2017-01-29 17:30:35 +0800

    todo: mongo

--- src/schema.js
+++ src/schema.js
@@ -1,27 +1,61 @@
 import {
   GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
+  GraphQLID,
   GraphQLInt,
+  GraphQLString,
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLNonNull,
 } from 'graphql';
+
+
+const Contact = new GraphQLObjectType({
+  name: 'Contact',
+  fields: {
+    _id: {type: GraphQLID},
+    phone: {type: GraphQLString},
+  }
+});
+const Me = new GraphQLObjectType({
+  name: 'Me',
+  fields: {
+    _id: {type: GraphQLID},
+    contacts: {type: new GraphQLList(Contact)},
+  }
+});
 
 
 const Query = new GraphQLObjectType({
   name: 'RootQuery',
   fields: {
-    // TODO: ...
+    me: {
+      type: Me,
+      args: {
+        _id: {type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve() {
+        // TODO: mongo
+        return {
+          _id: '568abef565368511002b9698',
+          contacts: [
+            {
+              _id: '1111111111111111111111',
+              phone: '13911592475',
+            },
+            {
+              _id: '2222222222222222222222222',
+              phone: '18618817397',
+            },
+          ],
+        }
+      }
+    }
   }
 });
-let Mutation = new GraphQLObjectType({
-  name: 'RootMutation',
-  fields: {
-    // TODO: ...
-  }
-})
+
 
 const Schema = new GraphQLSchema({
   query: Query,
-  mutation: Mutation,
 });
 
 export default Schema;
