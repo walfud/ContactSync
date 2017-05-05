c806c6197486a46638fe2b5b44632a10ee5d2667
walfud <hi@walfud.com>
2017-05-05 12:33:52 +0800

    token -> oid

--- services/network.js
+++ services/network.js
@@ -1,11 +1,21 @@
 const http = require('http');
+const fetch = require('node-fetch');
+const querystring = require('querystring');
 
 function getOauthId(token, refreshToken) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('oid1');
-        }, 1000);
+    const param = querystring.stringify({
+        client_id: 'df33ecd49de1c42e7107845d3b1ae1a0', 
+        token,
+        refresh_token: refreshToken,
     });
+    return fetch(`http://oauth2.walfud.com/oid?${param}`)
+        .then(async res => {
+            try {
+                return await res.json();
+            } catch (err) {
+                throw `${res.status}: ${res.statusText}\n${res.url}`;
+            }
+        });
 }
 
 module.exports = {
