7b907c4d3dead76824702501202a1850ba9792b2
walfud <hi@walfud.com>
2017-07-02 19:03:52 +0800

    cb use get request

--- routes/cb.js
+++ routes/cb.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router');
 const fetch = require('node-fetch');
+const querystring = require('querystring');
 
 const {
     OAUTH2_URL,
@@ -10,14 +11,20 @@ const router = new Router();
 /**
  * 转发给认证服务器
  */
-router.post('/cb', async (cxt, next) => {
+router.get('/cb', async (cxt, next) => {
+    const { code, state } = querystring.parse(cxt.request.url.substr(cxt.request.url.indexOf('?') + 1))
     cxt.body = await fetch(`${OAUTH2_URL}/token`, {
         method: 'POST',
         headers: {
-            'content-type': cxt.request.header['content-type'],
+            'content-type': 'application/x-www-form-urlencoded',
             'x-access-token': cxt.request.header['x-access-token'],
         },
-        body: cxt.request.rawBody,
+        body: querystring.stringify({
+            grant_type: 'authorization_code',
+            client_id: 'contactsync',
+            redirect_uri: '',
+            code,
+        }),
     })
         .then(res => res.text());
 });
